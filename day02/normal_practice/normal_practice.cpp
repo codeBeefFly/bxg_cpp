1c760d97749798693a4da49af7502e8a775c385e
codeBeefFly <lijin890119@163.com>
2020-08-11 17:27:18 +0800

    0811 update

---
+++ day02/normal_practice/normal_practice.cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+
+int practice_01() {
+
+	std::cout << "..in day02 practice_01...\n";
+
+	{
+		/// 需求
+		///给定一串字符串，判定里面有多少个数字，多少个字母。
+		///并且把最后那个字母a 修改为大写字母A 
+		///修改用的函数是：replace（修改起始索引位置，修改的横跨长度，修改成什 么字符）; 
+		///判断是否是字母和数字使用的是 isalpha() 和 isdigit(), 需要引入 #include<ctype.h>
+		
+	}
+
+	return 0;
+}
+
+
+int practice_02() {
+
+	std::cout << "..in day02 practice_02...\n";
+
+	{
+		/// 需求
+
+	}
+
+	return 0;
+}
+
+
+int practice_03() {
+
+	std::cout << "..in day02 practice_03...\n";
+	
+	{
+		/// 需求
+
+	}
+
+	return 0;
+}
+
+
+int main(){
+
+
+
+	return 0;
+}
