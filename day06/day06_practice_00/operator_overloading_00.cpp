a51b3ba78e7022194bdf956986b177748aeed0ab
codeBeefFly <lijin890119@163.com>
2020-08-26 14:21:41 +0800

    op. ov. class global

--- day06/day06_practice_00/operator_overloading_00.cpp
+++ day06/day06_practice_00/operator_overloading_00.cpp
@@ -2,6 +2,10 @@
 
 使用运算符重载，将将两个对象的相加，返回他们的年龄总和
 
+这段代码将要体现运算符重载的两种形式
+1. 运算符重载函数做类成员函数  operator+()
+2. 运算符重载函数做全局定义函数 operator-()
+
 */
 
 
@@ -22,11 +26,19 @@ public:
 		std::cout << "..有参构造函数...\n";
 	}
 
-	int operator+(Student& stu) {
+	int operator+(Student& stu) {				// +运算符重载，类成员函数形式	
 		return this->age + stu.age;
 	}
+
 };
 
+
+// -运算符重载，全局函数定义形式
+int operator-(Student stu1, Student stu2) {
+
+	return stu1.age - stu2.age;
+
+}
 
 
 int main() {
@@ -34,7 +46,8 @@ int main() {
 	std::cout << "..in operator_overloading_00...\n";
 
 	Student s1(10), s2(20);
-	std::cout << "..学生 s1 与 s2 年龄总和 :: " << s1 + s2 << "\n";	// 使用运算符重载
+	std::cout << "..学生 s1 与 s2 年龄总和 :: " << s1 + s2 << "\n";	// 使用运算符重载，类成员函数形式
+	std::cout << "..学生 s1 与 s2 年龄之差 :: " << s1 - s2 << "\n";	// 使用运算符重载，全局函数定义形式
 
 	return 0;
 }
@@ -48,5 +61,13 @@ output
 ..有参构造函数...
 ..有参构造函数...
 ..学生 s1 与 s2 年龄总和 :: 30
+
+
+
+..in operator_overloading_00...
+..有参构造函数...
+..有参构造函数...
+..学生 s1 与 s2 年龄总和 :: 30
+..学生 s1 与 s2 年龄之差 :: -10
 
 */
